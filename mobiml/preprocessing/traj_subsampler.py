bdbc3a29da4b6b72702a357ed82aab9a4e39734e
mar3021 <marinaa3021@gmail.com>
2024-07-02 12:04:44 +0200

    Change parameter name for clarity

--- mobiml/preprocessing/traj_subsampler.py
+++ mobiml/preprocessing/traj_subsampler.py
@@ -1,33 +1,35 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from mobiml.datasets._dataset import Dataset, TIMESTAMP, SPEED, TRAJ_ID, MOVER_ID
 from tqdm.auto import tqdm
 
 
-class TrajectorySubsampler():
-    def __init__(self, data:Dataset) -> None:
+class TrajectorySubsampler:
+    def __init__(self, data: Dataset) -> None:
         self.data = data
 
     def subsample(self, min_dt_sec=10) -> Dataset:
         tqdm.pandas()
-        df_clean = self.data.df.sort_values(TIMESTAMP, kind='mergesort') \
-            .groupby(TRAJ_ID) \
-            .progress_apply(
-                lambda l: self.subsample_trajectory(l.copy(), min_dt_sec
-            )
-        ).reset_index(level=0, drop=True)
+        df_clean = (
+            self.data.df.sort_values(TIMESTAMP, kind="mergesort")
+            .groupby(TRAJ_ID)
+            .progress_apply(lambda l: self._subsample_trajectory(l.copy(), min_dt_sec))
+            .reset_index(level=0, drop=True)
+        )
         self.data.df = df_clean
         return self.data
 
-    def subsample_trajectory(self, traj, min_dt_sec=10) -> pd.DataFrame:
+    def _subsample_trajectory(
+        self, traj_df: pd.DataFrame, min_dt_sec=10
+    ) -> pd.DataFrame:
         # Based on: https://stackoverflow.com/a/56904899
-        sumlm = np.frompyfunc(lambda a,b: a+b if a < min_dt_sec else b, 2, 1)
+        sumlm = np.frompyfunc(lambda a, b: a + b if a < min_dt_sec else b, 2, 1)
 
-        traj_dt = traj[TIMESTAMP].diff().dt.total_seconds()
+        traj_dt = traj_df[TIMESTAMP].diff().dt.total_seconds()
 
         import warnings
+
         with warnings.catch_warnings(record=True):
             traj_dt_sumlm = sumlm.accumulate(traj_dt, dtype=int)
 
-        return traj.drop(traj_dt_sumlm.loc[traj_dt_sumlm < min_dt_sec].index)
-    
+        return traj_df.drop(traj_dt_sumlm.loc[traj_dt_sumlm < min_dt_sec].index)
