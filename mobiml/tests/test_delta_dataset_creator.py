a5ac97d928ae98cad5a68d6819e88a0290afe8cd
mar3021 <marinaa3021@gmail.com>
2024-07-17 10:04:14 +0200

    Updates for tests

---
+++ mobiml/tests/test_delta_dataset_creator.py
@@ -0,0 +1,41 @@
+import os
+
+from mobiml.loaders import TemporalSplitter
+
+from mobiml.datasets import PreprocessedBrestAIS
+
+from mobiml.transforms import DeltaDatasetCreator
+
+
+class TestDeltaDatasetCreator:
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def test_delta_dataset(self):
+        path = os.path.join(
+            self.test_dir, "data/test_nautilus_trajectories_preprocessed_100.csv"
+        )
+        dataset = PreprocessedBrestAIS(path)
+        ais = TemporalSplitter(dataset).split()
+        delta_dataset = DeltaDatasetCreator(ais)
+        assert isinstance(delta_dataset, DeltaDatasetCreator)
+        traj_delta = delta_dataset.get_delta_dataset("split", njobs=1)
+
+        expected_dt_curr = [259, 80, 91]
+        dt_curr = traj_delta.dt_curr.tolist()
+        assert dt_curr[:3] == expected_dt_curr
+
+        traj_delta_windows = delta_dataset.get_windowed_dataset("split")
+        assert len(traj_delta_windows) == 1
+
+        expected_samples = [259, 80, 91]
+        samples_list = traj_delta_windows.samples.tolist()
+        assert samples_list[0][0][2] == expected_samples[0]
+        assert samples_list[0][1][2] == expected_samples[1]
+        assert samples_list[0][2][2] == expected_samples[2]
+        assert len(samples_list[0]) == 67
+
+        expected_labels = [81.82, -26.76]
+        labels_list = traj_delta_windows.labels.tolist()
+        assert round(labels_list[0][0], 2) == expected_labels[0]
+        assert round(labels_list[0][1], 2) == expected_labels[1]
+        assert len(labels_list[0]) == 2
---
+++ mobiml/tests/test_traj_aggregator.py
@@ -0,0 +1,80 @@
+import os
+import pandas as pd
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
+from datetime import datetime, timedelta
+
+from mobiml.datasets import MOVER_ID
+
+from mobiml.transforms import AISTripExtractor, TrajectoryAggregator
+
+
+class TestTrajectoryAggregator:
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def setup_method(self):
+        df = pd.DataFrame(
+            [
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 0, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                    "speed": 1,
+                    "direction": 90.0,
+                    "ship_type": "Passenger",
+                    "Name": "SHIP1",
+                    "client": 1,
+                },
+                {
+                    "geometry": Point(3, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 6, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                    "speed": 3,
+                    "direction": 180.0,
+                    "ship_type": "Passenger",
+                    "Name": "SHIP1",
+                    "client": 1,
+                },
+                {
+                    "geometry": Point(3, 6),
+                    "timestamp": datetime(2018, 1, 1, 12, 20, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                    "speed": 2,
+                    "direction": 270.0,
+                    "ship_type": "Passenger",
+                    "Name": "SHIP2",
+                    "client": 1,
+                },
+                {
+                    "geometry": Point(6, 9),
+                    "timestamp": datetime(2018, 1, 1, 12, 25, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                    "speed": 4,
+                    "direction": 90.0,
+                    "ship_type": "Passenger",
+                    "Name": "SHIP2",
+                    "client": 1,
+                },
+            ]
+        )
+        self.gdf = GeoDataFrame(df, crs=4326)
+
+    def test_aggregated_trajs(self):
+        h3_resolution = 2
+        vessels = self.gdf.groupby(MOVER_ID)[["ship_type", "Name"]].agg(pd.Series.mode)
+        trajs = AISTripExtractor(self.gdf).get_trips(gap_duration=timedelta(minutes=10))
+        trajs = TrajectoryAggregator(trajs, vessels).aggregate_trajs(h3_resolution)
+
+        expected_speed_median = [2, 3]
+        trajs_speed = trajs.speed_median.tolist()
+        assert trajs_speed[0] == expected_speed_median[0]
+        assert trajs_speed[1] == expected_speed_median[1]
+
+        expected_direction_start = [90.0, 270.0]
+        trajs_direction = trajs.direction_start.tolist()
+        assert trajs_direction[0] == expected_direction_start[0]
+        assert trajs_direction[1] == expected_direction_start[1]
