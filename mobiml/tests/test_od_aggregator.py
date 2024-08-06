e113814f20d993419b3f70e78da047363206edfe
mar3021 <marinaa3021@gmail.com>
2024-08-06 13:31:09 +0200

    Improve ODAggregator, including tests

---
+++ mobiml/tests/test_od_aggregator.py
@@ -0,0 +1,185 @@
+import os
+import pandas as pd
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
+from datetime import datetime
+
+from mobiml.datasets import Dataset
+from mobiml.transforms.od_aggregator import ODAggregator
+
+
+class TestODAggregator:
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def test_spatial_temporal_overlap(self):
+        df = pd.DataFrame(
+            [
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 0, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(3, 3),
+                    "timestamp": datetime(2018, 1, 1, 12, 6, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 10, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+                {
+                    "geometry": Point(3, 3),
+                    "timestamp": datetime(2018, 1, 1, 12, 15, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+            ]
+        )
+        self.gdf = GeoDataFrame(df, crs=4326)
+
+        dataset = Dataset(self.gdf)
+        assert len(dataset.to_trajs()) == 2
+        aggregator = ODAggregator(dataset)
+        assert isinstance(aggregator, ODAggregator)
+        data = aggregator.get_od_for_h3(7, "1D")
+        assert data.df.origin.sum() == 2
+        assert len(data.df.t.unique()) == 1
+        assert len(data.df.h3_cell.unique()) == 1
+        assert data.df.t[0] == datetime(2018, 1, 1, 0, 0, 0)
+        assert data.df.h3_cell[0] == "87754e64dffffff"
+
+    def test_temporal_overlap(self):
+        df = pd.DataFrame(
+            [
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 0, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(3, 3),
+                    "timestamp": datetime(2018, 1, 1, 12, 6, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(6, 6),
+                    "timestamp": datetime(2018, 1, 1, 12, 10, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+                {
+                    "geometry": Point(9, 9),
+                    "timestamp": datetime(2018, 1, 1, 12, 15, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+            ]
+        )
+        self.gdf = GeoDataFrame(df, crs=4326)
+
+        dataset = Dataset(self.gdf)
+        assert len(dataset.to_trajs()) == 2
+        aggregator = ODAggregator(dataset)
+        assert isinstance(aggregator, ODAggregator)
+        data = aggregator.get_od_for_h3(7, "1D")
+        assert data.df.origin.sum() == 2
+        assert len(data.df.t.unique()) == 1
+        assert len(data.df.h3_cell.unique()) == 2
+        assert data.df.t[0] == datetime(2018, 1, 1, 0, 0, 0)
+        h3_cell = ["87588a210ffffff", "87754e64dffffff"]
+        assert data.df.h3_cell.tolist() == h3_cell
+
+    def test_spatial_overlap(self):
+        df = pd.DataFrame(
+            [
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 0, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(3, 3),
+                    "timestamp": datetime(2018, 1, 1, 12, 6, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 3, 12, 10, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+                {
+                    "geometry": Point(3, 3),
+                    "timestamp": datetime(2018, 1, 3, 12, 15, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+            ]
+        )
+        self.gdf = GeoDataFrame(df, crs=4326)
+
+        dataset = Dataset(self.gdf)
+        assert len(dataset.to_trajs()) == 2
+        aggregator = ODAggregator(dataset)
+        assert isinstance(aggregator, ODAggregator)
+        data = aggregator.get_od_for_h3(7, "1D")
+        assert data.df.origin.sum() == 2
+        print(data.df)
+        assert len(data.df.t.unique()) == 2
+        assert len(data.df.h3_cell.unique()) == 1
+        t = [datetime(2018, 1, 1, 0, 0, 0), datetime(2018, 1, 3, 0, 0, 0)]
+        assert data.df.t.tolist() == t
+        assert data.df.h3_cell[0] == "87754e64dffffff"
+
+    def test_no_overlap(self):
+        df = pd.DataFrame(
+            [
+                {
+                    "geometry": Point(0, 0),
+                    "timestamp": datetime(2018, 1, 1, 12, 0, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(3, 3),
+                    "timestamp": datetime(2018, 1, 1, 12, 6, 0),
+                    "traj_id": 1,
+                    "mover_id": 1,
+                },
+                {
+                    "geometry": Point(6, 6),
+                    "timestamp": datetime(2018, 1, 3, 12, 10, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+                {
+                    "geometry": Point(9, 9),
+                    "timestamp": datetime(2018, 1, 3, 12, 15, 0),
+                    "traj_id": 2,
+                    "mover_id": 2,
+                },
+            ]
+        )
+        self.gdf = GeoDataFrame(df, crs=4326)
+
+        dataset = Dataset(self.gdf)
+        assert len(dataset.to_trajs()) == 2
+        aggregator = ODAggregator(dataset)
+        assert isinstance(aggregator, ODAggregator)
+        data = aggregator.get_od_for_h3(7, "1D")
+        assert data.df.origin.sum() == 2
+        assert len(data.df.t.unique()) == 2
+        assert len(data.df.h3_cell.unique()) == 2
+        t = [datetime(2018, 1, 1, 0, 0, 0), datetime(2018, 1, 3, 0, 0, 0)]
+        assert data.df.t.tolist() == t
+        h3_cell = ["87754e64dffffff", "87588a210ffffff"]
+        assert data.df.h3_cell.tolist() == h3_cell
--- mobiml/transforms/od_aggregator.py
+++ mobiml/transforms/od_aggregator.py
@@ -3,15 +3,37 @@ import pandas as pd
 import movingpandas as mpd
 from datetime import datetime
 
-from mobiml.datasets import Dataset
+from mobiml.datasets import Dataset, TRAJ_ID, MOVER_ID, TIMESTAMP
 
 
 class ODAggregator:
     def __init__(self, data: Dataset) -> None:
         self.data = data
 
-    def get_od_for_h3(self, res) -> Dataset:
+    def get_od_for_h3(self, res, freq) -> pd.DataFrame:
+        """
+        Extract start and end points (OD) for trajectories from a Dataset and aggregate them in H3.
+        H3 level and temporal binning are customizable.
+
+        Parameters
+        ----------
+        res : int
+            Desired number for the H3 resolution
+        freq : string
+            Desired frequency for temporal binning, guide: https://pandas.pydata.org/docs/user_guide/timeseries.html#offset-aliases
+
+        Returns
+        ----------
+        DataFrame with columns t, h3_cell, origin, destination
+
+        Examples
+        ----------
+        >>> taxis = PortoTaxis(r"../examples/data/train.csv", nrows=10000)
+        >>> od_h3 = ODAggregator(taxis).get_od_for_h3(7, "1h")
+        """
         gdf = self.data.to_gdf()
+        gdf = gdf[[TRAJ_ID, MOVER_ID, TIMESTAMP, "geometry"]]
+
         gdf["x"] = gdf.geometry.x
         gdf["y"] = gdf.geometry.y
 
@@ -20,44 +42,54 @@ class ODAggregator:
         gdf["h3_cell"] = gdf.apply(
             lambda row: str(h3.geo_to_h3(row.y, row.x, res)), axis=1
         )
-
         gdf = gdf[gdf.h3_cell != "0"]
 
-        gdf = gdf.drop(
-            columns={
-                "CALL_TYPE",
-                "ORIGIN_CALL",
-                "ORIGIN_STAND",
-                "DAY_TYPE",
-                "MISSING_DATA",
-            }
+        tc = mpd.TrajectoryCollection(
+            gdf, traj_id_col=TRAJ_ID, obj_id_col=MOVER_ID, t=TIMESTAMP, x="x", y="y"
         )
-
-        tc = mpd.TrajectoryCollection(gdf, "traj_id", t="timestamp", x="x", y="y")
 
         print(f"{datetime.now()} Getting start locations ...")
         start = tc.get_start_locations()
         start = start.rename(
             columns={
-                "timestamp": "start_t",
+                TIMESTAMP: "start_t",
                 "geometry": "origin",
                 "h3_cell": "h3_cell_origin",
             }
         )
-        start = start.drop(columns={"mover_id", "x", "y"})
+        start = start.drop(columns={MOVER_ID, "x", "y"})
+
+        start = start.set_index("start_t")
+        df = start.groupby([pd.Grouper(freq=freq), "h3_cell_origin"]).count()
+        df = df.origin
+        df = df.reset_index()
 
         print(f"{datetime.now()} Getting end locations ...")
         end = tc.get_end_locations()
         end = end.rename(
             columns={
-                "timestamp": "end_t",
+                TIMESTAMP: "end_t",
                 "geometry": "destination",
                 "h3_cell": "h3_cell_destination",
             }
         )
-        end = end.drop(columns={"traj_id", "mover_id", "x", "y"})
+        end = end.drop(columns={TRAJ_ID, MOVER_ID, "x", "y"})
 
-        od_h3 = pd.concat([start, end], axis=1)
+        end = end.set_index("end_t")
+        df1 = end.groupby([pd.Grouper(freq=freq), "h3_cell_destination"]).count()
+        df1 = df1.destination
+        df1 = df1.reset_index()
 
-        self.data.df = od_h3
+        combined = pd.concat([df, df1], axis=1)
+        combined["t"] = combined[["start_t", "end_t"]].max(axis=1)
+        combined["h3_cell"] = combined["h3_cell_origin"]
+        combined["h3_cell"] = combined["h3_cell"].fillna(
+            combined["h3_cell_destination"]
+        )
+        combined["origin"] = combined["origin"].fillna(0)
+        combined["origin"] = combined["origin"].astype(int)
+        combined["destination"] = combined["destination"].fillna(0)
+        combined = combined[["t", "h3_cell", "origin", "destination"]]
+
+        self.data.df = combined
         return self.data
