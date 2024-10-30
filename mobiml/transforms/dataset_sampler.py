bb648675c1fc9bcdbca7713f5eba61a78d930501
mar3021 <marinaa3021@gmail.com>
2024-10-30 14:44:48 +0100

    Add dataset sampling, incl. tests

--- mobiml/transforms/dataset_sampler.py
+++ mobiml/transforms/dataset_sampler.py
@@ -1,6 +1,8 @@
 import numpy as np
 import geopandas as gpd
 import shapely
+import math
+import warnings
 
 from mobiml.datasets import Dataset
 
@@ -10,18 +12,24 @@ class RandomTrajSampler:
     def __init__(self, data: Dataset) -> None:
         self.data = data
 
-    def random_sample(self, n_cells, n_sample) -> Dataset:
+    def random_sample(
+        self, n_cells, n_sample=None, percent_sample=None, random_state=None, **kwargs
+    ) -> Dataset:
         """
         Randomly samples trajectories in a region of interest.
-        Based on https://github.com/microsoft/torchgeo
+        Based on: https://github.com/microsoft/torchgeo
         and https://james-brennan.github.io/posts/fast_gridding_geopandas/
 
         Parameters
         ----------
-        n_cells : int | float
-                Desired number of cells per row
+        n_cells : int | (int, int)
+                Desired number of cells per row and column. Square layout if only one value provided.
         n_sample : int
-                Desired sample number from each cell
+                Desired sample number from dataset.
+        percent_sample : float
+                Desired percentage from dataset for sample.
+        random_state : int
+                Seed for random number generator.
 
         Returns
         ----------
@@ -30,7 +38,7 @@ class RandomTrajSampler:
         Examples
         ----------
         >>> data = AISDK(r"../examples/data/aisdk_20180208_sample.zip")
-        >>> random_sample = RandomTrajSampler(data).random_sample(2, 20)
+        >>> random_sample = RandomTrajSampler(data).random_sample(n_cells=2, n_sample=100)
         """
 
         trajs = self.data.to_trajs()
@@ -50,11 +58,8 @@ class RandomTrajSampler:
             elif isinstance(value, int):
                 cell_size_x = (xmax - xmin) / value
                 cell_size_y = (ymax - ymin) / value
-            elif isinstance(value, float):
-                cell_size_x = (xmax - xmin) / value
-                cell_size_y = (ymax - ymin) / value
             else:
-                print("Please provide a tuple, int or float.")
+                print("Please provide a tuple or int.")
             return cell_size_x, cell_size_y
 
         cell_size_x, cell_size_y = stride(n_cells)
@@ -72,40 +77,78 @@ class RandomTrajSampler:
         merged = gpd.sjoin(start, cell, how="left", predicate="within")
         merged = merged.drop(columns="index_right")
 
-        def calc_sample_size(n_sample):
-            if n_sample < 1:
-                n_sample = int(n_sample * len(merged))
-                return n_sample
-            else:
-                n_sample = int(n_sample)
-                return n_sample
+        filled_cells = merged.cell.unique()
+        all_cells = merged.merge(cell, how="outer").cell.unique().tolist()
+        diff = [cell for cell in all_cells if cell not in filled_cells]
+        if not diff:
+            print("All cells can be used for sampling.")
+        else:
+            warnings.warn(
+                "There are empty cells that will not be used for sampling.", UserWarning
+            )
+            print("Empty cells:", diff)
 
-        n_sample = calc_sample_size(n_sample)
+        print("Number of cells used for sampling:", len(filled_cells))
 
-        def get_cell_sample(n_sample):
+        def get_cell_sample(
+            n_sample=None, percent_sample=None, random_state=None, **kwargs
+        ):
+            if percent_sample:
+                n_sample = percent_sample * len(merged)
+
+            if n_sample > len(start):
+                try:
+                    raise ValueError("Sample too big.")
+                except ValueError:
+                    print(
+                        "Your sample of",
+                        n_sample,
+                        "cannot be greater than the dataset:",
+                        len(start),
+                    )
+                    raise
+
+            sample_size = n_sample / len(filled_cells)
+            n_sample = math.ceil(sample_size)
+            print("Number of samples per cell:", n_sample)
+
             if n_sample > merged.cell.value_counts().min():
-                print(
-                    "Your cell sample of",
-                    n_sample,
-                    "cannot be greater than the minimum number of points in a cell:",
-                    merged.cell.value_counts().min(),
+                num = merged["cell"].value_counts()
+                count = 0
+                for n in num:
+                    if n < n_sample:
+                        count += 1
+                    else:
+                        pass
+                warnings.warn(
+                    "There are not enough points in some cells, so all points in these cells will be used for sampling.",
+                    UserWarning,
                 )
-                print("Setting the cell sample to:", merged.cell.value_counts().min())
-                n_sample = merged.cell.value_counts().min()
-            df_sample = merged.groupby("cell").sample(n=n_sample)
+                print("Not enough points in", count, "cell(s).")
+
+            df_sample = merged.groupby(
+                ["cell"], as_index=False, group_keys=False
+            ).apply(
+                lambda x: x.sample(min(n_sample, len(x)), random_state=random_state),
+                include_groups=False,
+            )
             df_sample["split"] = 2
             df_sample = df_sample[["traj_id", "split"]]
             combined = merged.merge(df_sample, how="left")
             combined.loc[combined["split"] != 2, "split"] = 1
             return combined
 
-        combined = get_cell_sample(n_sample)
+        combined = get_cell_sample(n_sample, percent_sample, random_state)
 
         dataset = Dataset(combined)
         return dataset
 
-    def get_sample_data(self, n_cells, n_sample) -> Dataset:
-        data = self.random_sample(n_cells, n_sample)
+    def get_sample_data(
+        self, n_cells, n_sample=None, percent_sample=None, random_state=None, **kwargs
+    ) -> Dataset:
+        data = self.random_sample(
+            n_cells, n_sample, percent_sample, random_state, **kwargs
+        )
 
         gdf = data.to_gdf()
 
