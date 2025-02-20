57ee1f2e841b4cc1d1540519f86d3c5ed114f11c
anitagraser <anita.graser@ait.ac.at>
2025-02-20 11:30:00 +0100

    Add temporal splitter, fixes #45

--- mobiml/preprocessing/traj_splitter.py
+++ mobiml/preprocessing/traj_splitter.py
@@ -1,4 +1,4 @@
-from movingpandas import ObservationGapSplitter
+from movingpandas import ObservationGapSplitter, TemporalSplitter
 from mobiml.datasets import Dataset
 from .utils import trajectorycollection_to_df
 
@@ -8,12 +8,33 @@ class TrajectorySplitter:
         self.data = data
 
     def split(self, **kwargs) -> Dataset:
-        observation_gap = kwargs.pop("observation_gap", None)
+        """
+        Split trajectories by different rules.
+
+        Parameters
+        ----------
+        observation_gap : datetime.timedelta
+            Time gap threshold.
+        temporal_split_mode : string
+            Split mode. ('hour', 'day', 'month' or 'year')
+
+        Examples
+        --------
+
+        >>> TrajectorySlitter(dataset).split(observation_gap=timedelta(hours=1))
+
+        >>> TrajectorySlitter(dataset).split(temporal_split_mode='day')
+        """
+
+        split_by_observation_gap = kwargs.pop("observation_gap", None)
+        split_temporally = kwargs.pop("temporal_split_mode", None)
 
         trajs = self.data.to_trajs()
 
-        if observation_gap:
-            trajs = ObservationGapSplitter(trajs).split(gap=observation_gap)
+        if split_by_observation_gap:
+            trajs = ObservationGapSplitter(trajs).split(gap=split_by_observation_gap)
+        if split_temporally:
+            trajs = TemporalSplitter(trajs).split(mode=split_temporally)
 
         df = trajectorycollection_to_df(trajs)
         self.data.df = df
