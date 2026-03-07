85399b79ebabb30bc7c344f8f8324c564b53d272
Anita Graser <anitagraser@gmx.at>
2026-03-07 19:26:26 +0100

    Improve test

--- tests/test_copenhagen_cyclists.py
+++ tests/test_copenhagen_cyclists.py
@@ -19,11 +19,23 @@ class TestCopenhagenCyclists:
     def test_coordinates_are_within_frame_bounds(self):
         path = os.path.join(self.test_dir, "data/test_bike.pickle")
         data = CopenhagenCyclists(path)
+        assert isinstance(data, CopenhagenCyclists)
+
+        assert len(data.df) == 4219       
+        assert TRAJ_ID in data.df.columns
+        assert TIMESTAMP in data.df.columns
+        assert COORDS in data.df.columns
+        assert ROWNUM in data.df.columns
+        assert MOVER_ID not in data.df.columns
+        
         x = get_x_from_xy(data.df)
         y = get_y_from_xy(data.df)
         assert x.between(0, 640).all(), "x coordinates out of bounds [0, 640]"
         assert y.between(0, 360).all(), "y coordinates out of bounds [0, 360]"
 
+        trajs = data.to_trajs()
+        assert isinstance(trajs, TrajectoryCollection)
+        
     def test_drop_extra_cols(self):
         path = os.path.join(self.test_dir, "data/test_bike.pickle")
         data = CopenhagenCyclists(path)
