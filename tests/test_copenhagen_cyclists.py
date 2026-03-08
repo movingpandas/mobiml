fd6382623b5d6acf1dac6442bffc927a8f3eadf6
Anita Graser <anitagraser@gmx.at>
2026-03-08 20:15:26 +0100

    improve test

--- tests/test_copenhagen_cyclists.py
+++ tests/test_copenhagen_cyclists.py
@@ -16,32 +16,31 @@ from mobiml.datasets.utils import get_x_from_xy, get_y_from_xy
 class TestCopenhagenCyclists:
     test_dir = os.path.dirname(os.path.realpath(__file__))
 
-    def test_coordinates_are_within_frame_bounds(self):
+    def setup_method(self):
         path = os.path.join(self.test_dir, "data/test_bike.pickle")
-        data = CopenhagenCyclists(path)
-        assert isinstance(data, CopenhagenCyclists)
+        self.data = CopenhagenCyclists(path)
 
-        assert len(data.df) == 4219
-        assert TRAJ_ID in data.df.columns
-        assert TIMESTAMP in data.df.columns
-        assert COORDS in data.df.columns
-        assert ROWNUM in data.df.columns
-        assert MOVER_ID not in data.df.columns
+    def test_coordinates_are_within_frame_bounds(self):
+        assert isinstance(self.data, CopenhagenCyclists)
+        assert len(self.data.df) == 4219
+        assert TRAJ_ID in self.data.df.columns
+        assert TIMESTAMP in self.data.df.columns
+        assert COORDS in self.data.df.columns
+        assert ROWNUM in self.data.df.columns
+        assert MOVER_ID not in self.data.df.columns
 
-        x = get_x_from_xy(data.df)
-        y = get_y_from_xy(data.df)
+        x = get_x_from_xy(self.data.df)
+        y = get_y_from_xy(self.data.df)
         assert x.between(0, 640).all(), "x coordinates out of bounds [0, 640]"
         assert y.between(0, 360).all(), "y coordinates out of bounds [0, 360]"
         assert x.iloc[0] == pytest.approx(483.545)
         assert y.iloc[0] == pytest.approx(181.87)
 
-        trajs = data.to_trajs()
+        trajs = self.data.to_trajs()
         assert isinstance(trajs, TrajectoryCollection)
         assert len(trajs) > 0
 
     def test_drop_extra_cols(self):
-        path = os.path.join(self.test_dir, "data/test_bike.pickle")
-        data = CopenhagenCyclists(path)
         extra_cols = {
             "frame_out",
             "num_frames",
@@ -53,4 +52,4 @@ class TestCopenhagenCyclists:
             "class",
         }
         for col in extra_cols:
-            assert col not in data.df.columns
+            assert col not in self.data.df.columns
